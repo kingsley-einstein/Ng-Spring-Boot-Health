2e6f50daaf13f12ff7a5c72d7a854f0f1658dd8d
Kingsley Victor <javaprodigy56@gmail.com>
2019-01-19 16:04:47 +0100

    New commit

---
+++ Health-App-Server/src/main/java/com/health/services/models/HealthProfile.java
@@ -0,0 +1,29 @@
+package com.health.services.models;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "profiles")
+@Entity
+public class HealthProfile implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "symptoms")
+    private String symptoms;
+
+    protected HealthProfile() {}
+
+    public HealthProfile(String symptoms) {
+        this.symptoms = symptoms;
+    }
+}
--- Health-App-Server/src/main/java/com/health/services/models/User.java
+++ Health-App-Server/src/main/java/com/health/services/models/User.java
@@ -23,17 +23,26 @@ public class User implements java.io.Serializable {
     @Size(max = 30, min = 5, message = "Invalid username")
     @NotNull(message = "Username cannot be null")
     @NotEmpty(message = "Username cannot be left empty")
-    @Column(name = "username")
+    @Column(name = "username", unique = true)
     private String username;
 
     @NotNull(message = "Email cannot be null")
     @NotEmpty(message = "Email cannot be left empty")
     @Email(message = "Not a valid email")
+    @Column(name = "email", unique = true)
     private String email;
 
     @NotNull(message = "Password cannot be null")
     @NotEmpty(message = "password cannot be empty")
     private String password;
+
+    protected User() {}
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     public Long getId() {
         return id;
@@ -47,7 +56,7 @@ public class User implements java.io.Serializable {
         return email;
     }
 
-    private String getPassword() {
+    public String getPassword() {
         return password;
-    }
+    } 
 }

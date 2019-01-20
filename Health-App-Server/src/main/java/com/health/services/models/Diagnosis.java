0d3cb24fa986abfa0b331b5a39742133ebdd48e5
Kingsley Victor <javaprodigy56@gmail.com>
2019-01-20 16:55:41 +0100

    New commit

--- Health-App-Server/src/main/java/com/health/services/models/Diagnosis.java
+++ Health-App-Server/src/main/java/com/health/services/models/Diagnosis.java
@@ -1,5 +1,17 @@
 package com.health.services.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table(name = "diagnosis")
+@Entity
 public class Diagnosis implements java.io.Serializable {
-    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }

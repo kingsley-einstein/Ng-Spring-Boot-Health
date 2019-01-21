d09269d6bc53abdb353465bd891f1e1fe209c01b
Kingsley Victor <javaprodigy56@gmail.com>
2019-01-21 22:26:45 +0100

    New commit

---
+++ Health-App-Server/src/main/java/com/health/services/controllers/PictureController.java
@@ -0,0 +1,10 @@
+package com.health.services.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+@RequestMapping("/picture")
+public class PictureController {
+    
+}

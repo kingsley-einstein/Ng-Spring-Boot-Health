87e99e2959bf9d95c2ec640e7da8efd8ae5c21c4
Kingsley Victor <javaprodigy56@gmail.com>
2019-01-25 04:54:46 +0100

    Made little change

--- Health-App-Server/src/main/java/com/health/services/controllers/PictureController.java
+++ Health-App-Server/src/main/java/com/health/services/controllers/PictureController.java
@@ -1,10 +1,29 @@
 package com.health.services.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.health.services.repositories.HealthProfileRepository;
+import com.health.services.repositories.PictureRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RestController
 @RequestMapping("/picture")
 public class PictureController {
     
+    @Autowired
+    private PictureRepository pictureRepo;
+
+    @Autowired
+    private HealthProfileRepository profileRepo;
+
+    @PostMapping
+    @ResponseBody
+    public String upload(@RequestParam("id") Long id) {
+        return "null";
+    }
 }
